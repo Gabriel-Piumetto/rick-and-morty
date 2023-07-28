f0a2b44b73966f27c9fec048b5ebe6b86542c5c8
Gabriel Piumetto <gabo125@gmail.com>
2023-07-28 02:56:03 -0300

    se hacen los cambios de la HW de ORM parte 1

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,13 @@
 const server = require('./app')
 const PORT = 3001
+const {conn} = require('./DB_connection')
 
 
+conn.sync({force:false}).then(value=>{
 
+    server.listen(PORT, ()=>{
+        console.log(`Server raised in port: ${PORT}`)
+    })
 
-server.listen(PORT, ()=>{
-    console.log(`Server raised in port: ${PORT}`)
-})
+}).catch(err => console.error(err))
+
